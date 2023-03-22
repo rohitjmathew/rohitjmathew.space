bb50b8566e764b24d23e24e53220a730e1031776
glocore <mailtoashwinm@gmail.com>
2023-03-22 16:25:31 +0530

    remove preact from production build

--- next.config.js
+++ next.config.js
@@ -22,15 +22,6 @@ module.exports = {
       require('./scripts/generate-sitemap');
     }
 
-    // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat'
-      });
-    }
-
     return config;
   }
 };
